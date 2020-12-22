783380609be86411b6a9cd2693d40fc53c519fc8
Your Name Hereagusrio <asubandrio@gmail.com>
2020-12-22 15:52:22 +0700

    Adding first code change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demoRS';
+  title = 'demoRS Aplication by Rizal Laksamana Hady';
 }
